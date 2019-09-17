bd470a3abb34a946eeca45104fef5773e5cb75b0
tosaka-n <tosquitoz@gmail.com>
2019-09-17 16:45:59 +0900

    add: convert password cli

--- encrypt.js
+++ encrypt.js
@@ -3,18 +3,15 @@ const algorithm = 'aes-256-cbc';
 require('dotenv').config();
 const env = process.env;
 const key = env.key;
-const PASSWORD = process.argv[2];
-function encrypt(text) {
+exports.encrypt = (text) => {
   const cipher = crypto.createCipher(algorithm, key)
   let crypted = cipher.update(text, 'utf8', 'base64')
   crypted += cipher.final('base64');
   return crypted;
 }
-function decrypt(text) {
+exports.decrypt = (text) => {
   const decipher = crypto.createDecipher(algorithm, key)
   let dec = decipher.update(text, 'base64', 'utf8')
   dec += decipher.final('utf8');
   return dec;
 }
-console.log(`encrypt: ${encrypt(PASSWORD)}`);
-console.log(`decrypt: ${decrypt(encrypt(PASSWORD))}`);
